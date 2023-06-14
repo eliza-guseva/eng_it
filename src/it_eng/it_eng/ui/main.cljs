efb29d3d7c5ec3ca9bddc6fa9dcf25666661dd44
Elizaveta Guseva <liza.guseva@hey.com>
2023-06-14 20:33:49 +0200

    release 0.41

--- src/it_eng/it_eng/ui/main.cljs
+++ src/it_eng/it_eng/ui/main.cljs
@@ -12,18 +12,22 @@
 
 (defn section-why-us []
   [:section.why-us
+   
    [:h2.why-us-title "Почему мы?"]
+
    [:div.why-us-cards
   [:div1.whyus-div [:p main-content/whyus1]]
   [:div2.whyus-div [:p main-content/whyus2]]
   [:div3.whyus-div [:p main-content/whyus3]]
   [:div4.whyus-div [:p main-content/whyus4]]]
+   
   [:div.why-us-cards
    [:div5.whyus-div [:p main-content/whyus5]]
   [:div6.whyus-div [:p main-content/whyus6]]
   [:div7.whyus-div [:p main-content/whyus7]]
-  [:div8.whyus-div [:p main-content/whyus8]]]]
-  )
+  [:div8.whyus-div [:p main-content/whyus8]]]
+   
+   ])
 
 (defn main []
     [:div.main
