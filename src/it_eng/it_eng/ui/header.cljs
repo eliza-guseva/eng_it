1a467363884fe47d41721db50712f059d5d820ea
Elizaveta Guseva <liza.guseva@hey.com>
2023-06-14 20:11:08 +0200

    more stuff

--- src/it_eng/it_eng/ui/header.cljs
+++ src/it_eng/it_eng/ui/header.cljs
@@ -4,10 +4,11 @@
   [:div.navbar
    [:ul.navbar-nav
     [:li.nav-item [:a.nav-link "Home"]]
-    [:li.nav-item [:a.nav-link "About"]]
-    [:li.nav-item [:a.nav-link "Contact"]]]])
+    [:li.nav-item [:a.nav-link "Курсы и цены"]]
+    [:li.nav-item [:a.nav-link "Контакты"]]
+    [:li.nav-item [:a.nav-link "Блог"]]]])
 
 (defn header []
   [:header.page-header 
-   [:h1.header-title "Английский для специалистов в ИТ"]
-   [navbar] ])
+   [navbar]
+   ])
--- src/it_eng/it_eng/ui/main.cljs
+++ src/it_eng/it_eng/ui/main.cljs
@@ -1,7 +1,26 @@
-(ns it-eng.it-eng.ui.main)
+(ns it-eng.it-eng.ui.main
+  [:require [it-eng.it-eng.ui.main-content :as main-content]])
+
+(defn section-image-headline []
+  [:section.image-headline
+   [:div.header-content-left
+    [:h1.header-title "Английский для специалистов в ИТ"]
+    [:h2.header-subtitle "Интервью в европейских и американских компаниях без боли и страха."]]
+   [:div.header-content-right
+    [:img.header-img {:src "images/head_image.jpg"}]]]
+  )
+
+(defn section-why-us []
+  [:section.why-us
+   [:h2.why-us-title "Почему мы?"]
+   [:div.why-us-cards
+  [:div1.whyus-div [:p main-content/whyus1]]
+  [:div2.whyus-div [:p main-content/whyus2]]
+  [:div3.whyus-div [:p main-content/whyus3]]
+  [:div4.whyus-div [:p main-content/whyus4]]]]
+  )
 
 (defn main []
-  (let [text "В этом разделе все про Английский язык для специалистов в ИТ."]
     [:div.main
-     [:h2.test
-      text]]))
+     [section-image-headline]
+     [section-why-us]])
