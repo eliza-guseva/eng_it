a378d816ef2324a84170380b2aba41eb4bfbb6b0
Elizaveta Guseva <liza.guseva@hey.com>
2023-06-14 20:24:39 +0200

    release 0.3

--- src/it_eng/it_eng/ui/main.cljs
+++ src/it_eng/it_eng/ui/main.cljs
@@ -18,6 +18,11 @@
   [:div2.whyus-div [:p main-content/whyus2]]
   [:div3.whyus-div [:p main-content/whyus3]]
   [:div4.whyus-div [:p main-content/whyus4]]]]
+  [:div.why-us-cards
+   [:div5.whyus-div [:p main-content/whyus5]]
+  [:div6.whyus-div [:p main-content/whyus6]]
+  [:div7.whyus-div [:p main-content/whyus7]]
+  [:div8.whyus-div [:p main-content/whyus8]]]
   )
 
 (defn main []
