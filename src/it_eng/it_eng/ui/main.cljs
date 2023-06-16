33011fae2332ad50287ee7eae53f936764273d28
Elizaveta Guseva <liza.guseva@hey.com>
2023-06-16 22:17:00 +0200

    a little bit of mess

--- src/it_eng/it_eng/ui/main.cljs
+++ src/it_eng/it_eng/ui/main.cljs
@@ -2,21 +2,27 @@
   [:require [it-eng.it-eng.ui.main-content :as main-content]])
 
 
+(defn whyus-text [reason]
+  [:p  {:style {:margin "1px"}} reason])
+
+
 (defn whyus-toprow []
   [:div.whyus-toprow
-   [:div.whyus-top1
-    [:p main-content/whyus1]]
    [:div.whyus-header
     [:h1.whyus-header "Почему мы"]]
+   [:div.whyus-top1
+    (whyus-text main-content/whyus1)]
    ])
+
+
 
 
 (defn whyus-bottomrow []
   [:div.whyus-bottomrow
-   [:div.whyus-2
-    [:p main-content/whyus2]]
-   [:div.whyus-3
-    [:p main-content/whyus3]]])
+   [:div.whyus-top3
+    (whyus-text main-content/whyus2)]
+   [:div.whyus-top3
+    (whyus-text main-content/whyus3)]])
 
 (defn section-why-us []
   [:section.why-us
